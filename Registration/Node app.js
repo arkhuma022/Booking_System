b311630db1ccb2bef0f5a8f695c7dd5d3e8a4752
arkhuma022 <arkhuma022@student.wethinkcode.co.za>
2023-09-18 15:13:13 +0200

    booking system to be finalized

--- Registration/Node app.js
+++ Registration/Node app.js
@@ -1,43 +1,43 @@
-//// ...
-//
-//// Route to add a new doctor
-//app.post('/api/doctors', (req, res) => {
-//    const newDoctor = req.body;
-//    newDoctor.id = doctors.length + 1; // Generate a unique ID for the new doctor
-//    doctors.push(newDoctor);
-//    res.json(newDoctor);
-//});
-//
-//// Route to get a list of all doctors
-//app.get('/api/doctors', (req, res) => {
-//    res.json(doctors);
-//});
-//
-//// Route to update an existing doctor's profile
-//app.put('/api/doctors/:id', (req, res) => {
-//    const doctorId = parseInt(req.params.id);
-//    const updatedDoctor = req.body;
-//
-//    const index = doctors.findIndex((doc) => doc.id === doctorId);
-//    if (index === -1) {
-//        res.status(404).json({ error: 'Doctor not found' });
-//    } else {
-//        doctors[index] = { ...doctors[index], ...updatedDoctor };
-//        res.json(doctors[index]);
-//    }
-//});
-//
-//// Route to delete a doctor by ID
-//app.delete('/api/doctors/:id', (req, res) => {
-//    const doctorId = parseInt(req.params.id);
-//    const index = doctors.findIndex((doc) => doc.id === doctorId);
-//
-//    if (index === -1) {
-//        res.status(404).json({ error: 'Doctor not found' });
-//    } else {
-//        const deletedDoctor = doctors.splice(index, 1);
-//        res.json(deletedDoctor[0]);
-//    }
-//});
-//
-//// ...
+// ...
+
+// Route to add a new doctor
+app.post('/api/doctors', (req, res) => {
+    const newDoctor = req.body;
+    newDoctor.id = doctors.length + 1; // Generate a unique ID for the new doctor
+    doctors.push(newDoctor);
+    res.json(newDoctor);
+});
+
+// Route to get a list of all doctors
+app.get('/api/doctors', (req, res) => {
+    res.json(doctors);
+});
+
+// Route to update an existing doctor's profile
+app.put('/api/doctors/:id', (req, res) => {
+    const doctorId = parseInt(req.params.id);
+    const updatedDoctor = req.body;
+
+    const index = doctors.findIndex((doc) => doc.id === doctorId);
+    if (index === -1) {
+        res.status(404).json({ error: 'Doctor not found' });
+    } else {
+        doctors[index] = { ...doctors[index], ...updatedDoctor };
+        res.json(doctors[index]);
+    }
+});
+
+// Route to delete a doctor by ID
+app.delete('/api/doctors/:id', (req, res) => {
+    const doctorId = parseInt(req.params.id);
+    const index = doctors.findIndex((doc) => doc.id === doctorId);
+
+    if (index === -1) {
+        res.status(404).json({ error: 'Doctor not found' });
+    } else {
+        const deletedDoctor = doctors.splice(index, 1);
+        res.json(deletedDoctor[0]);
+    }
+});
+
+// ...
